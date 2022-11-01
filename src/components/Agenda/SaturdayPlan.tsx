0f25b5777b8cdef45f7c354d3a30e6ef8ce67e3a
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2022-11-01 18:13:54 +0100

    extract component

--- src/components/Agenda/SaturdayPlan.tsx
+++ src/components/Agenda/SaturdayPlan.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import Section from './Section';
 
 function SaturdayPlan() {
     const { t } = useTranslation()
@@ -6,20 +7,14 @@ function SaturdayPlan() {
     return (
         <>
             <p className='section-title'>Hotel Check-In</p>
-            <div className='section'>
-                <div className='key'>what</div>
-                <div className='content'>
-                    <p>If you are staying with us, you need to check-in at your arrival. You will receive your room keys and the complete schedule for the weekend.</p>
-                </div>
-            </div>
+            <Section name="what">
+                <p>If you are staying with us, you need to check-in at your arrival. You will receive your room keys and the complete schedule for the weekend.</p>
+            </Section>
             <p className='section-title'>Welcome Cocktail & Granitas</p>
-            <div className='section'>
-                <div className='key'>time</div>
-                <div className='content'>
-                    <p>Rooms will be ready from 2pm.</p>
-                    <p>Arrived too early? No problem! You can leave your luggage at the desk and chill in the pool.</p>
-                </div>
-            </div>
+            <Section name="time">
+                <p>Rooms will be ready from 2pm.</p>
+                <p>Arrived too early? No problem! You can leave your luggage at the desk and chill in the pool.</p>
+            </Section>
             <p className='section-title'>Cerimony</p>
             <p className='section-title'>Antipasti</p>
             <p className='section-title'>Dinner</p>
