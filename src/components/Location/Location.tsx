bbc7b5114071bd80dceec9f413522409d150ed4d
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2022-11-04 09:26:33 +0100

    updated structure of location page

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,11 +1,11 @@
 import './Location.css';
-import airport from './airport.png';
-import wedding from './wedding.png';
+import dimora1 from './dimora-1.png';
+import dimora2 from './dimora-2.png';
+import dimora3 from './dimora-3.png';
 
 import { useTranslation } from 'react-i18next';
-import Map from './Map'
-import Marker from './Marker'
 import { Button } from '@mui/material';
+import Map from './Map'
 
 function Location() {
     const { t } = useTranslation()
@@ -13,22 +13,42 @@ function Location() {
     return (
         <div id="location" className="page">
             <div className="page-title">{t('location-title')}</div>
-            <div className="location-description-1">{t('location-description-1')}</div>
-            <div className="location-description-2">{t('location-description-2')}</div>
-            <div className="location-description-3">{t('location-description-3')}</div>
-            <div className="location-amenities">
-                {t('location-amenities')}
-                <li><span>{t('location-amenities-1')}</span></li>
-                <li><span>{t('location-amenities-2')}</span></li>
-                <li><span>{t('location-amenities-3')}</span></li>
-                <li><span>{t('location-amenities-4')}</span></li>
-                <li><span>{t('location-amenities-5')}</span></li>
+            <div className="location-name">{t('location-name')}</div>
+            <div className="location-description">{t('location-description')}</div>
+
+            <div className="staying">
+                <span>{t('location-stay')}</span>
+                <img src={dimora1} />
+                <div className="location-description-1">{t('location-description-1')}</div>
+                <div className="location-description-2">{t('location-description-2')}</div>
+                <div className="location-description-3">{t('location-description-3')}</div>
+                <div className="location-description-4">{t('location-description-4')}</div>
+                <div className="location-amenities">
+                    {t('location-amenities')}
+                    <li><span>{t('location-amenities-1')}</span></li>
+                    <li><span>{t('location-amenities-2')}</span></li>
+                    <li><span>{t('location-amenities-3')}</span></li>
+                    <li><span>{t('location-amenities-4')}</span></li>
+                    <li><span>{t('location-amenities-5')}</span></li>
+                </div>
             </div>
-            <div className="section-title">How to get there</div>
-            <div className="get-there-1">If you come by plane, we can arrange a transfer from Comiso or Catania airport.</div>
-            <div className="get-there-2">If you are staying in another place nearby, we can arrange a transfer from there to La Dimora and vice-versa.</div>
-            <div className="get-there-3">If you prefer to rent a car, La Dimora is a 23-minute drive from Comiso Airport and a 1h30-hour drive from Catania Airport. </div>
-            <Button className="button-secondary" variant="outlined" target="_blank" href='https://goo.gl/maps/yce7DY31rtnVvMqz7'>Get directions</Button>
+
+            <div className="directions">
+                <span>{t('location-get-there')}</span>
+                <img src={dimora2} />
+                <img src={dimora3} />
+                <div className="location-get-there-1">{t('location-get-there-1')}</div>
+                <div className="location-get-there-2">{t('location-get-there-2')}</div>
+                <div className="location-get-there-3">{t('location-get-there-3')}</div>
+                <Button
+                    className="button-secondary"
+                    variant="outlined"
+                    target="_blank"
+                    href='https://goo.gl/maps/yce7DY31rtnVvMqz7'>
+                    {t('location-get-directions')}
+                </Button>
+            </div>
+
             <Map zoom={9} center={[37.1156893, 14.7667613]} />
         </div>
     )
