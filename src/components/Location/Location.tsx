cc0c781f094e2117a834a045e8a0d25857d1dcce
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2022-11-04 09:53:15 +0100

    mobile

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -16,37 +16,43 @@ function Location() {
             <div className="location-name">{t('location-name')}</div>
             <div className="location-description">{t('location-description')}</div>
 
-            <div className="staying">
-                <span>{t('location-stay')}</span>
-                <img src={dimora1} />
-                <div className="location-description-1">{t('location-description-1')}</div>
-                <div className="location-description-2">{t('location-description-2')}</div>
-                <div className="location-description-3">{t('location-description-3')}</div>
-                <div className="location-description-4">{t('location-description-4')}</div>
-                <div className="location-amenities">
-                    {t('location-amenities')}
-                    <li><span>{t('location-amenities-1')}</span></li>
-                    <li><span>{t('location-amenities-2')}</span></li>
-                    <li><span>{t('location-amenities-3')}</span></li>
-                    <li><span>{t('location-amenities-4')}</span></li>
-                    <li><span>{t('location-amenities-5')}</span></li>
+            <img className="main-img" src={dimora1} />
+            <div className="section">
+                <span className="section-title">{t('location-stay')}</span>
+                <div className="sectionContent">
+                    <div className="location-description-1">{t('location-description-1')}</div>
+                    <div className="location-description-2">{t('location-description-2')}</div>
+                    <div className="location-description-3">{t('location-description-3')}</div>
+                    <div className="location-description-4">{t('location-description-4')}</div>
+                    <div className="location-amenities">
+                        {t('location-amenities')}
+                        <li><span>{t('location-amenities-1')}</span></li>
+                        <li><span>{t('location-amenities-2')}</span></li>
+                        <li><span>{t('location-amenities-3')}</span></li>
+                        <li><span>{t('location-amenities-4')}</span></li>
+                        <li><span>{t('location-amenities-5')}</span></li>
+                    </div>
                 </div>
             </div>
 
-            <div className="directions">
-                <span>{t('location-get-there')}</span>
+            <div className="img-line">
                 <img src={dimora2} />
                 <img src={dimora3} />
-                <div className="location-get-there-1">{t('location-get-there-1')}</div>
-                <div className="location-get-there-2">{t('location-get-there-2')}</div>
-                <div className="location-get-there-3">{t('location-get-there-3')}</div>
-                <Button
-                    className="button-secondary"
-                    variant="outlined"
-                    target="_blank"
-                    href='https://goo.gl/maps/yce7DY31rtnVvMqz7'>
-                    {t('location-get-directions')}
-                </Button>
+            </div>
+            <div className="section">
+                <span className="section-title">{t('location-get-there')}</span>
+                <div className="sectionContent">
+                    <div className="location-get-there-1">{t('location-get-there-1')}</div>
+                    <div className="location-get-there-2">{t('location-get-there-2')}</div>
+                    <div className="location-get-there-3">{t('location-get-there-3')}</div>
+                    <Button
+                        className="button-secondary"
+                        variant="outlined"
+                        target="_blank"
+                        href='https://goo.gl/maps/yce7DY31rtnVvMqz7'>
+                        {t('location-get-directions')}
+                    </Button>
+                </div>
             </div>
 
             <Map zoom={9} center={[37.1156893, 14.7667613]} />
