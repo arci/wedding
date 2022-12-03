7ea5c6add9cb4388a6a98ea525e4b7a54680f89d
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2022-12-03 12:29:43 +0100

    add material ui accordion

--- src/components/FAQs/FAQs.tsx
+++ src/components/FAQs/FAQs.tsx
@@ -1,6 +1,8 @@
 import './FAQs.css';
 
 import { useTranslation } from 'react-i18next';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function FAQs() {
     const { t } = useTranslation()
@@ -8,7 +10,21 @@ function FAQs() {
     return (
         <div id="faqs" className="page">
             <div className="page-title">{t('faqs-title')}</div>
-            <p className='coming-soon'>{t('coming-soon')}</p>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Accordion 1</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
         </div>
     )
 }
