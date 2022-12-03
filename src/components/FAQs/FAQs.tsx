d5659e63ae3e8196a0c980a33eaf891a6c6868f2
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2022-12-03 12:57:22 +0100

    defined basic structure and style

--- src/components/FAQs/FAQs.tsx
+++ src/components/FAQs/FAQs.tsx
@@ -3,26 +3,73 @@ import './FAQs.css';
 import { useTranslation } from 'react-i18next';
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState } from 'react';
 
 function FAQs() {
     const { t } = useTranslation()
 
+    const [expanded, setExpanded] = useState<string | false>(false);
+
+    const handleChange = (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+        setExpanded(isExpanded ? panel : false);
+    };
+
     return (
         <div id="faqs" className="page">
             <div className="page-title">{t('faqs-title')}</div>
-            <Accordion>
+            <div className="section-title">The Wedding</div>
+            <Accordion elevation={0} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1a-content"
-                    id="panel1a-header"
+                    id="panel1"
                 >
-                    <Typography>Accordion 1</Typography>
+                    Accordion 1
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </AccordionDetails>
+            </Accordion>
+            <Accordion elevation={0} expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel2"
+                >
+                    Accordion 1
+                </AccordionSummary>
+                <AccordionDetails>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </AccordionDetails>
+            </Accordion>
+            
+            <div className="section-title">Getting There</div>
+            <Accordion elevation={0} expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel3"
+                >
+                    Accordion 1
+                </AccordionSummary>
+                <AccordionDetails>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </AccordionDetails>
+            </Accordion>
+            <Accordion elevation={0} expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel4"
+                >
+                    Accordion 1
+                </AccordionSummary>
+                <AccordionDetails>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
                 </AccordionDetails>
             </Accordion>
         </div>
