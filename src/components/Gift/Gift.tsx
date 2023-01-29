a382b6b2bccb6de9e05913964a33a0e377a05c1d
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2023-01-29 11:47:39 +0100

    basic page structure

--- src/components/Gift/Gift.tsx
+++ src/components/Gift/Gift.tsx
@@ -1,4 +1,6 @@
 import './Gift.css';
+import head from './images/head.png';
+import tail from './images/tail.png';
 
 import { useTranslation } from 'react-i18next';
 
@@ -8,7 +10,24 @@ function Gift() {
     return (
         <div id="gift" className="page">
             <div className="page-title">{t('gift-title')}</div>
-            <p className='coming-soon'>{t('coming-soon')}</p>
+            <div className="gift-name">{t('gift-name')}</div>
+            <div className="gift-description">{t('gift-description')}</div>
+
+            <img className="main-img" src={head} alt="Past travels"/>
+
+            <div className="section">
+                <div className="location-description-1">{t('location-description-1')}</div>
+                <div className="location-description-2">{t('location-description-2')}</div>
+                <div className="location-description-3">{t('location-description-3')}</div>
+                <div className="location-description-4">{t('location-description-4')}</div>
+                <div className="location-description-4">{t('location-description-4')}</div>
+            </div>
+
+            <img className="main-img" src={tail} alt="Future travels"/>
+
+            <div className="section">
+                <div className="location-description-1">{t('location-description-1')}</div>
+            </div>
         </div>
     )
 }
