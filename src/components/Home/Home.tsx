6803f3b09a3b9cf4d20983c32308843e3b7782ac
fabio.arcidiacono <fabio.arcidiacono@mollie.com>
2023-04-15 17:57:33 +0200

    correct agenda link

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -14,7 +14,7 @@ function Home() {
             <div className="who">{t('who')}</div>
             <div className="when">{t('when')}</div>
             <div className="where">{t('where')}</div>
-            <Link to="/rsvp">
+            <Link to="/agenda">
                 <Button className="button-primary" variant="outlined">{t('agenda')}</Button>
             </Link>
         </div>
